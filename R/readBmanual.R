d9fcfc23592782683e51160b09921cde5920e028
JonathanYe3 <jonathanye2003@gmail.com>
2020-07-07 16:03:06 -0400

    Fixed major issues by implementing exception/error handling. Now, this function will not break if it encounters an html error or if an html node does not exist. Instead, it will toss an NA into the data.

--- R/readBmanual.R
+++ R/readBmanual.R
@@ -20,33 +20,36 @@
 #' @export
 readBmanual <- function(a,b){
 
-      x <- c(a:b)
-      Bergeys <- c("https://onlinelibrary.wiley.com/doi/10.1002/9781118960608.gbm") %>%
-            paste0(sprintf("%05d",x))
+   x <- c(a:b)
+   Bergeys <- c("https://onlinelibrary.wiley.com/doi/10.1002/9781118960608.gbm") %>%
+      paste0(sprintf("%05d",x))
 
-      #Get genus
-      extract_genus <- function(url){
-            data <- xml2::read_html(url)
-            f_genus <- data %>%
-                  rvest::html_nodes(".citation__title i") %>%
-                  rvest::html_text() %>%
-                  tibble::as_tibble()
-      }
+   #Get genus
+   extract_genus <- function(url){
+      data <- xml2::read_html(url)
+      f_genus <- data %>%
+         rvest::html_nodes(".citation__title i:nth-child(1)") %>%
+         rvest::html_text() %>%
+         {if(length(.) == 0) NA else .}
+   }
 
-      #Get paragraph
-      extract_paragraph <- function(url){
-            data <- xml2::read_html(url)
-            f_paragraph <- data %>%
-                  rvest::html_nodes("#section-1-en p:nth-child(3)") %>%
-                  rvest::html_text() %>%
-                  tibble::as_tibble()
-      }
+   #Get paragraph
+   extract_paragraph <- function(url){
+      data <- xml2::read_html(url)
+      f_paragraph <- data %>%
+         rvest::html_nodes("#section-1-en p:nth-child(3)") %>%
+         rvest::html_text() %>%
+         {if(length(.) == 0) NA else .}
+   }
 
-      genus_name <- purrr::map_dfr(Bergeys, extract_genus)
-      paragraph_info <- purrr::map_dfr(Bergeys, extract_paragraph)
+   safe_genus <- purrr::possibly(extract_genus, otherwise = NA)
+   safe_abstract <- purrr::possibly(extract_paragraph, otherwise = NA)
 
-      my_df <- data.frame(genus_name, paragraph_info) %>%
-            mutate_if(is.character, trimws)
+   genus_name <- purrr::map(Bergeys, safe_genus)
+   paragraph_info <- purrr::map(Bergeys, safe_abstract) %>%
+      trimws(which = c("left"))
+   my_df <- tibble::tibble(genus = genus_name,
+                           abstract = paragraph_info)
 
-      return(my_df)
+
 }
